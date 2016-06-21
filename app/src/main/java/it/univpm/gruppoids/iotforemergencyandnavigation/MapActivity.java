b3a35d21c11e665484dcf8a2597617c87e8c0a94
Simone Bufarini <simone.bufarini@outlook.com>
2016-06-21 02:19:07 +0200

    02.19 21.06

--- app/src/main/java/it/univpm/gruppoids/iotforemergencyandnavigation/MapActivity.java
+++ app/src/main/java/it/univpm/gruppoids/iotforemergencyandnavigation/MapActivity.java
@@ -7,9 +7,11 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 //import com.polites.android.GestureImageView;
 
 public class MapActivity extends AppCompatActivity {
@@ -46,6 +48,17 @@ public class MapActivity extends AppCompatActivity {
                     break;
             }
         }
+
+        mapImage.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    String text = "You click at x = " + event.getX() + " and y = " + event.getY();
+                    Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+        });
     }
 
     @Override
