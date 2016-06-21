d9db8d9af9f41db964ac2800a72c910e112e4b25
Simone Bufarini <simone.bufarini@outlook.com>
2016-06-21 02:23:53 +0200

    02.23 21.06

--- app/src/main/java/it/univpm/gruppoids/iotforemergencyandnavigation/MapActivity.java
+++ app/src/main/java/it/univpm/gruppoids/iotforemergencyandnavigation/MapActivity.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-//import com.polites.android.GestureImageView;
 
 public class MapActivity extends AppCompatActivity {
 
